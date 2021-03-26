db5bdce1d8cab241fb9939937e22f16c1202219b
Araf020 <Araf020@users.noreply.github.com>
2021-03-26 17:58:24 +0600

    Parser Done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import Editor.Editor;
+import Editor.EditorFactory;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
+
         System.out.println("Hello World!");
+
+        Scanner in = new Scanner(System.in);
+
+        String ext =  in.nextLine();
+
+        System.out.println(ext);
+
+        Editor editor = EditorFactory.getEditor(ext);
+
+
+
+
     }
 
 
